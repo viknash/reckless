dbf536857cb342a28f32932da26f28d3c7128620
Mattias Flodin <git@codepentry.com>
2015-06-19 22:23:55 +0200

    wip

--- reckless/src/output_buffer.cpp
+++ reckless/src/output_buffer.cpp
@@ -111,7 +111,7 @@ void output_buffer::write(void const* buf, std::size_t count)
     pcommit_end_ += remaining_input;
 }
 
-std::error_code output_buffer::flush() noexcept
+void output_buffer::flush() noexcept
 {
     // TODO keep track of a high watermark, i.e. max value of pcommit_end_.
     // Clear every second or some such. Use madvise to release unused memory.
@@ -133,40 +133,99 @@ std::error_code output_buffer::flush() noexcept
     // not want to end up with half-written frames in the middle of the log
     // file. So, we only write data up until the end of the last complete input
     // frame.
-    std::size_t count = pframe_end_ - pbuffer_;
-    // NOTE if you get a crash here, it could be because your log object has a
-    // longer lifetime than the writer (i.e. the writer has been destroyed
-    // already).
-    std::error_code ec;
-    std::size_t written;
-    try {
-        written = pwriter_->write(pbuffer_, count, ec)
-    } catch(...) {
-        // It is a fatal error for the writer to throw an exception, because we
-        // can't tell how much data was written to the target before the
-        // exception occurred. Errors should be reported via the error code
-        // parameter.
-        // TODO assign a more specific error code to this so the client can now
-        // what went wrong.
-        ec.assign(writer::permanent_failure, writer::error_category());
-        written = 0;
+    std::size_t remaining = pframe_end_ - pbuffer_;
+    unsigned block_time_ms = 0;
+    while(true) {
+        std::error_code error;
+        std::size_t written;
+        try {
+            // NOTE if you get a crash here, it could be because your log object has a
+            // longer lifetime than the writer (i.e. the writer has been destroyed
+            // already).
+            written = pwriter_->write(pbuffer_, remaining, error)
+        } catch(...) {
+            // It is a fatal error for the writer to throw an exception, because we
+            // can't tell how much data was written to the target before the
+            // exception occurred. Errors should be reported via the error code
+            // parameter.
+            // TODO assign a more specific error code to this so the client can now
+            // what went wrong.
+            error.assign(writer::permanent_failure, writer::error_category());
+            written = 0;
+        }
+        if(likely(!error))
+            assert(written == count);   // A successful writer must write *all* data.
+        else
+            assert(written <= count);   // A failing writer may write all data, some data, or no data (but no more than that).
+        
+        // Discard the data that was written, preserve data that remains. We could
+        // avoid this copy by using a circular buffer, but in practice it should be
+        // rare to have any data remaining in the buffer. It only happens if the
+        // buffer fills up entirely (forcing us to flush in the middle of a frame)
+        // or if there is an error in the writer.
+        std::size_t remaining_data = (pcommit_end_ - pbuffer_) - written;
+        std::memmove(pbuffer_, pbuffer_+written, remaining_data);
+        pframe_end_ -= written;
+        pcommit_end_ -= written;
+
+        if(likely(!error)) {
+            if(unlikely(lost_input_frames_)) {
+                // FIXME notify about lost frames
+                lost_input_frames_ = 0;
+            }
+        } else {
+            error_policy ep;
+            if(ec == writer::temporary_error) {
+                error_state = writer::temporary_error;
+                ep = temporary_error_policy_;
+            } else {
+                error_state = writer::permanent_error;
+                ep = permanent_error_policy_;
+            }
+
+            switch(ep) {
+            case ignore:
+                throw flush_error_discard_frame();
+            case notify_on_recovery:
+                // We will notify the client about this once the writer
+                // starts working again.
+                ++lost_input_frames_;
+                throw flush_error_discard_frame();
+            case block:
+                // To give the client the appearance of blocking, we need to poll the
+                // writer, i.e. check periodically whether writing is now working, until it
+                // starts working again. We don't remove anything from the input queue while
+                // this happens, hence any client threads that are writing log events will
+                // start blocking once the input queue fills up. We use
+                // shared_input_queue_full_event_ for an exponentially increasing wait time
+                // between polls. That way we can check the panic-flush flag early, which
+                // will be set in case the program crashes.
+                //
+                // If the program crashes while the writer is failing (not an unlikely
+                // scenario since circumstances are already ominous), then we have a
+                // dilemma. We could keep on blocking, but then we are withholding a
+                // crashing program from generating a core dump until the writer starts
+                // working. Or we could just throw the input queue away and pretend we're
+                // done with the panic flush, so the program can die in peace. But then we
+                // will lose log data that might be vital to determining the cause of the
+                // crash. I've chosen the latter option, because I think it's not likely
+                // that the log data will ever make it past the writer anyway, even if we do
+                // keep on blocking.
+                shared_input_queue_full_event_.wait(block_time_ms);
+                if(panic_flush_) {
+                    on_panic_flush_done();
+                    return error_handling_action::abort;
+                }
+                block_time_ms += std::max(1u, block_time_ms/4);
+                block_time_ms = std::min(block_time_ms, 1000u);
+                break;
+            case fail_immediately:
+                // FIXME
+                return error_handling_action::abort;
+            }
+            return false;
+        }
     }
-    if(likely(!ec))
-        assert(written == count);   // A successful writer must write *all* data.
-    else
-        assert(written <= count);   // A failing writer may write all data, some data, or no data (but no more than that).
-    
-    // Discard the data that was written, preserve data that remains. We could
-    // avoid this copy by using a circular buffer, but in practice it should be
-    // rare to have any data remaining in the buffer. It only happens if the
-    // buffer fills up entirely (forcing us to flush in the middle of a frame)
-    // or if there is an error in the writer.
-    std::size_t remaining_data = (pcommit_end_ - pbuffer_) - written;
-    std::memmove(pbuffer_, pbuffer_+written, remaining_data);
-    pframe_end_ -= written;
-    pcommit_end_ -= written;
-    
-    return ec;
 }
 
 char* output_buffer::reserve_slow_path(std::size_t size)
